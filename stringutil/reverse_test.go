2e291ad226b6bdeffc3ffc1854288a15f9ba36a2
諦念 <teinen.github@gmail.com>
2018-08-07 22:21:02 +0900

    Implement test

--- stringutil/reverse_test.go
+++ stringutil/reverse_test.go
@@ -3,5 +3,18 @@ package stringutil
 import "testing"
 
 func TestReverse(t *testing.T) {
+	cases := []struct {
+		actual, expect string
+	}{
+		{"Hello, world", "dlrow ,olleH"},
+		{"Hello, 世界", "界世 ,olleH"},
+		{"", ""},
+	}
 
+	for _, c := range cases {
+		got := Reverse(c.actual)
+		if got != c.expect {
+			t.Errorf("Reverse(%q) == %q, expect %q", c.actual, got, c.expect)
+		}
+	}
 }
